c6b4eb3396ec0a824156791ca6b4d58e87170204
Chengyang Li <35854014+chengyangliii@users.noreply.github.com>
2021-02-10 13:47:05 -0500

    Add files via upload

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,25 +3,22 @@ import re
 
 def alphabet_validate(token):
     for i in token:
-        if ( i >= u'\u0041' and i <= u'\u005A' ) or ( i >= u'\u0061' and i <= u'\u007A'):
-            continue
-        else:
+        if not ( i >= u'\u0061' and i <= u'\u007A'):
             return False
     return True
 
 def Preprocessing():
-    file = open("./Trec_microblog11.txt", mode='r', encoding='UTF-8-sig')
+    file = open("./Trec_microblog11.txt", 'r')
     sp = spacy.load('en_core_web_sm')
     token_array = []
     line = file.readline()
-    
     while line:
         temp = []
         sentence = sp(line)
         for token in sentence:
             if not token.is_stop and not token.is_punct and not token.is_space and token.text[:7]!='http://':
-                if not alphabet_validate(token.text):
-                    result = re.findall(r'[a-zA-Z]+', token.text)
+                if not alphabet_validate(token.lower_):
+                    result = re.findall(r'[a-z]+', token.lower_)
                     for i in result:
                         if len(i) > 2:
                             temp.append(i.lower())
@@ -31,7 +28,6 @@ def Preprocessing():
         temp.insert(0, line.split()[0])
         token_array.append(temp)
         line = file.readline()
-
     file.close()
     print(token_array)
 
