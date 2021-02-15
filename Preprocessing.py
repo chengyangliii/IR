256ecbc075a86df0b7ae772660ca5f488fe172d1
Chengyang Li <35854014+chengyangliii@users.noreply.github.com>
2021-02-14 22:03:54 -0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,34 +1,41 @@
 import spacy
 import re
+import time
 
-def alphabet_validate(token):
-    for i in token:
-        if not ( i >= u'\u0061' and i <= u'\u007A'):
-            return False
-    return True
+def queryToTokens(queryString,sp=spacy.load('en_core_web_sm')):
+    tokens = []
+    sentence = sp(queryString)
+    for token in sentence:
+        if not token.is_stop and not token.is_punct and not token.is_space and not token.like_url:
+            if (not token.is_alpha or not token.is_ascii):
+                result = re.findall(r'[a-z]+', token.lemma_)
+                for i in result:
+                    if len(i) > 2:
+                        tokens.append(i.lower())
+                continue
+            if len(token.text) > 2:
+                tokens.append(token.lemma_.lower())
+    return(tokens)
 
 def Preprocessing():
-    file = open("./Trec_microblog11.txt", 'r')
-    sp = spacy.load('en_core_web_sm')
+    start = time.time()
+    file = open("./Trec_microblog11.txt", mode='r', encoding='UTF-8-sig')
     token_array = []
+    sp = spacy.load('en_core_web_sm')
     line = file.readline()
+    count = 0
     while line:
-        temp = []
-        sentence = sp(line)
-        for token in sentence:
-            if not token.is_stop and not token.is_punct and not token.is_space and token.text[:7]!='http://':
-                if not alphabet_validate(token.lower_):
-                    result = re.findall(r'[a-z]+', token.lower_)
-                    for i in result:
-                        if len(i) > 2:
-                            temp.append(i.lower())
-                    continue
-                if len(token.text) > 2:
-                    temp.append(token.lemma_.lower())
-        temp.insert(0, line.split()[0])
-        token_array.append(temp)
+        count+=1
+        if (count%1000 == 0):
+            print("Processing line {}".format(str(count)),end="\r")
+        tokens = queryToTokens(line,sp)
+        tokens.insert(0, line.split()[0])
+        # print(tokens)
+        token_array.append(tokens)
         line = file.readline()
     file.close()
-    print(token_array)
+    print("\nFinished preprocessing!")
+    print("Time:%ss"%(time.time()-start))
+    return(token_array)
 
 Preprocessing()
